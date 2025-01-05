83f6150d87b6b7d757faf5d354f0125c69b73ddc
unknown <manoranjanjenamitsy17@gmail.com>
2025-01-05 15:33:35 +0530

    Created DB In MongoDB

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # crudmern
+
+npx create-react-app .
+npm install web-vitals
+
+
+https://www.youtube.com/watch?v=hmwFN5YzY1s
+12:00 mongodb 
+
+
+s
---
+++ backend/models/userModel.js
@@ -0,0 +1,27 @@
+const mongoose=require("mongoose")
+
+// Create Schema
+const userSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+
+    },
+    email:{
+        type:String,
+        unique:true,
+        required:true
+    },
+    age:{
+        type:Number,
+
+    }
+})
+
+// create Model 
+const User = mongoose.model('User',userSchema )
+module.exports=User
+
+
+//Schema only defines the structure of your document
+//Model is : - We need to convert our blogSchema into model  we can work with to do that we pass it into mongoose.model(modelName,schema)
